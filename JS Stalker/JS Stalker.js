3881b32f0628fbed80438ef757c8d4fadde643f9
Vincent Liow <34018885+VLiow@users.noreply.github.com>
2018-10-29 22:39:40 -0400

    Add files via upload

---
+++ JS Stalker/JS Stalker.js
@@ -0,0 +1,9 @@
+
+var firstname,lastname,age;
+
+firstname = prompt("What is your first name?");
+lastname = prompt("What is your last name?");
+age = prompt("What is your age?");
+
+console.log("Your name is " + firstname + " " + lastname);
+console.log("You are " age " years old")
